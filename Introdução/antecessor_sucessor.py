0d1b9977161f02608554a75dc43779cf0d2a99ff
Rodrigo Ferrão <91308830+RodrigoFerrao@users.noreply.github.com>
2022-04-20 03:08:19 -0300

    Create antecessor_sucessor.py

---
+++ Introdução/antecessor_sucessor.py
@@ -0,0 +1,11 @@
+"""
+6) Faça um programa que leia um número inteiro e mostre o seu antecessor e seu
+sucessor.
+Ex:
+Digite um número: 9
+O antecessor de 9 é 8
+O sucessor de 9 é 10
+"""
+
+numero = int(input("Digite um número que deseje saber seu antecessor e seu sucessor\n"))
+print(f"O número escolhido foi {numero}\n seu antecessor é {numero-1}\n seu sucessor é {numero+1}")
