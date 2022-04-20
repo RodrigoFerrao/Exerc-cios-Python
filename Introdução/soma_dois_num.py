7ee0d48cc79844472e07dafd793cb9d99e277a5c
Rodrigo Ferrão <91308830+RodrigoFerrao@users.noreply.github.com>
2022-04-20 02:48:51 -0300

    Create soma_dois_num.py

---
+++ Introdução/soma_dois_num.py
@@ -0,0 +1,17 @@
+"""
+4) Desenvolva um algoritmo que leia dois números inteiros e mostre o somatório
+entre eles.
+Ex:
+Digite um valor: 8
+Digite outro valor: 5
+A soma entre 8 e 5 é igual a 13.
+"""
+
+print("Olá, seja bem-vindo a calculadora básica de soma")
+num1 = float(input("Digite o primeiro número\n"))
+num2 = float(input("Digite o segundo número\n"))
+num3 = num1 + num2
+print(f"O seu primeiro número foi {num1}, o seu segundo número foi {num2} e a soma entre eles é {num3}")
+print("Obrigado por usar a calculadora simples!")
+
+
